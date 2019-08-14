2b3b84dcd8eaf60538d6f9bb9c505bc1943a72a0
Mia Tabladillo <52267579+mianoelani@users.noreply.github.com>
2019-08-14 07:49:12 -0700

    Create Hw1-1.java

---
+++ Hw1-1.java
@@ -0,0 +1,20 @@
+/**
+ * Print Numbers in a table
+ *
+ * @MiaTabladillo
+ * @8/14/2019
+ */
+public class hw
+{
+    private int x;
+    /**
+     * prints numbers in a list
+     */
+    public hw()
+    {
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+        
+        x = 0;
+    }
